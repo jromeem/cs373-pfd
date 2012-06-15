696fb93e0de1658d0fc13f3d4beaf1e7eb6bdcef
Michael Pace <mpace1027@gmail.com>
2012-06-15 01:44:03 -0500

    fixed brute force pfd_eval method (IT NOW WORKS!!!), left lengthy comment

--- SpherePFD.py
+++ SpherePFD.py
@@ -60,42 +60,49 @@ def pfd_solve (r, w):
     pred_list = p[0]
     succ_list = p[1]
 
-    print "pred_list", pred_list
-    print "succ_list", succ_list
+    #print "pred_list", pred_list
+    #print "succ_list", succ_list
     pfd_eval(pred_list, succ_list)
 
 def pfd_eval (pred_list, succ_list):
     zero_pred = []
+    output_string = ""
 
     for vert in range(1, len(pred_list)):
         if pred_list[vert] == []:
             zero_pred.append(vert)
 
-
-
-    
-    for vert in zero_pred:
+    # NOTES TO SELVES:
+    # below, originally had for vert in zero_pred: instead of while loop, but
+    # this didn't work, since it looks like the for loop structure only
+    # checked the elements of zero_pred before beginning to iterate, but
+    # not after each iteration.
+    # so as a while loop, we're safe, since it continues to check that condition.
+    while len(zero_pred):
+        vert = zero_pred[0]
         pred_count = 0
         min_vert = 0
         min_vert_count = 200
         
         for succ in succ_list[vert]:
-            print pred_list
-            
-            pred_count = len(pred_list[vert])
+            pred_count = len(pred_list[succ])
 
             if pred_count < min_vert_count:
                 min_vert = succ
                 min_vert_count = pred_count
-
-            pred_list[succ].remove(vert)              
+            
+            pred_list[succ].remove(vert)  
 
             if len(pred_list[succ]) == 0:
                 zero_pred.append(succ)
                 #print vert
         
-        print vert
+        output_string += str(vert) + " "
         zero_pred.remove(vert)
+        
+    print output_string[:-1]
+    #print "zero_pred", zero_pred
+    #print "pred_list", pred_list
 
 # ----
 # main
